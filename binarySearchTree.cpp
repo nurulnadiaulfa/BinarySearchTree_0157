503e317e9db6a61bf7a65305419de82b1e6755fe
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-06-26 10:22:25 +0700

    menambahkan Step 7 : If the value in the data field of the new node is greater than that of the parent  di void insert class binarysearchtree

--- binarySearchTree.cpp
+++ binarySearchTree.cpp
@@ -66,6 +66,15 @@ public:
             //6.b : Exit
             return;
         }
+        // Step 7 : If the value in the data field of the new node is greater than that of the parent 
+        else if (x > parent->info)
+        {
+            //7a : Make the right child of parent point to the new node
+            parent->rightchild = newNode;
+
+            //7b : Exit 
+            return;
+        }
 
     }
 }
