b2a483c3d38cc75fbae7d3b3d3333ef91720ac12
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-06-26 10:09:00 +0700

    menambahkan Step 3 : Make the left and right child of the new node point to null di void insert class binanry tree

--- binarySearchTree.cpp
+++ binarySearchTree.cpp
@@ -37,5 +37,11 @@ public:
 
         //Step 2 : Assign value to the data field of new node
         newNode->info = x;
+
+        //Step 3 : Make the left and right child of the new node point to null
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
+        
     }
 }
