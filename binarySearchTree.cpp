89e3faa6d5559df86992d292158748bd736fda20
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-06-26 10:06:19 +0700

    menambahkan Step 1 : Allocate memory for the new node di void insert class binanrytree

--- binarySearchTree.cpp
+++ binarySearchTree.cpp
@@ -31,5 +31,8 @@ public:
         int x;
         cout << "Masukkan nilai : ";
         cin >> x;
+
+        //Step 1 : Allocate memory for the new node
+        Node *newNode = new Node();
     }
 }
