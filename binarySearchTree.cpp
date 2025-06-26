080a9e46f24a68dbc43834828029c0c1d5b41b1c
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-06-26 09:59:36 +0700

    menambahkan class node public

--- binarySearchTree.cpp
+++ binarySearchTree.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+class Node
+{
+public: 
+    int info;
+    Node *leftchild;
+    Node *rightchild;
+
+    //Constructor for the node class
+    Node()
+    {
+        leftchild = nullptr; // Initialize left child to null
+        rightchild = nullptr; // initialize right child to null
+    }
+};
