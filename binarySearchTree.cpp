d6e5913a7507f87338111da5d6b531ec817fe2a2
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-06-26 10:07:23 +0700

    menambahkan Step 2 : Assign value to the data field of new node di class binarytree

--- binarySearchTree.cpp
+++ binarySearchTree.cpp
@@ -34,5 +34,8 @@ public:
 
         //Step 1 : Allocate memory for the new node
         Node *newNode = new Node();
+
+        //Step 2 : Assign value to the data field of new node
+        newNode->info = x;
     }
 }
